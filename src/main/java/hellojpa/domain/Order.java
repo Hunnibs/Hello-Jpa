79b9072132373772809ad092003d08962e67b0a3
Hunnibs <cqqudgjs@hufs.ac.kr>
2023-03-16 22:40:01 +0900

    Setting

---
+++ src/main/java/hellojpa/domain/Order.java
@@ -0,0 +1,50 @@
+package hellojpa.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+    private LocalDateTime orderDate;
+    private OrderStatus status;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
